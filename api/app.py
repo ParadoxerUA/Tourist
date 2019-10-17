1b6a04d7afe7cbdedfc13bb6d2910066240cb8a4
Bogdan Lomako <lomakojob@gmail.com>
2019-10-18 00:11:52 +0300

    fixed structure

--- api/app.py
+++ api/app.py
@@ -9,10 +9,18 @@ def _setup_blueprints(main_app, app_list):
         main_app.register_blueprint(app, url_prefix=f'/api/{app.name}')
 
 
+def _set_db(main_app):
+    from database import db
+    # import your models below
+
+    db.init_app(main_app)
+
+
 def create_app(config):
     main_app = Flask(__name__)
     main_app.config.from_object(config)
     _setup_blueprints(main_app, APPS)
+    _set_db(main_app)
     return main_app
 
 
--- api/manage.py
+++ api/manage.py
@@ -2,13 +2,11 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from database import db
 from app import create_app
-# import your models below
 
 
 if __name__ == '__main__':
     from config import DebugConfig
     app = create_app(DebugConfig)
-    db.init_app(app)
     migrate = Migrate(app, db)
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
