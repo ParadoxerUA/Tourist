fe44b31635408d34a9a2077fbc7da118cdfcfb41
Kirill Redin <redinkirill.ua@gmail.com>
2019-10-21 19:28:04 +0300

    Add TripSchema for trip data validation

--- api/apps/trip_app/schemas/trip_schema.py
+++ api/apps/trip_app/schemas/trip_schema.py
@@ -1,4 +1,6 @@
-from marshmallow import Schema, fields, pprint
+from marshmallow import Schema, fields, validates_schema, ValidationError
+import datetime
+
 
 class TripSchema(Schema):
     name = fields.Str()
@@ -6,3 +8,12 @@ class TripSchema(Schema):
     start_date = fields.Date()
     end_date = fields.Date()
     status = fields.Bool()
+
+    @validates_schema
+    def validate_date(self, data, **kwargs):
+        start_date = data['start_date']
+        end_date = data['end_date']
+        if start_date > end_date:
+            raise ValidationError("Start date can not be greater than end date")
+        else:
+            print("HELLO\n"*20)
