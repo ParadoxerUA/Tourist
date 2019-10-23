560241129282ca67018c4b185dc61ac23b49ee6d
Kirill Redin <redinkirill.ua@gmail.com>
2019-10-23 16:11:42 +0300

    Add Point model. Edit foreign key in Trip model.

--- api/apps/trip_app/models.py
+++ api/apps/trip_app/models.py
@@ -7,10 +7,11 @@ class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(200))
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
     status = db.Column(db.Boolean)
-    id_admin = db.Column(db.Integer, db.ForeignKey('user_profile.id'))
+    id_admin = db.Column(db.Integer, db.ForeignKey('user_profile.id'), nullable=False)
+    trip = db.relationship("UserProfile", foreign_keys=[id_admin])
 
     @classmethod
     def create_trip(cls, data):
@@ -23,4 +24,23 @@ class Trip(db.Model):
         return f'<Trip {self.name}>'
 
 
+class Point(db.Model):
+    __tablename__ = 'point'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    id_trip = db.Column(db.Integer, db.ForeignKey('trip.id'), nullable=False)
+    trip = db.relationship("Trip", foreign_keys=[id_trip])
 
+    @classmethod
+    def create_point(cls, data):
+        point = cls(**data)
+        db.session.add(point)
+        db.session.commit()
+        return point
+
+
+    def __repr__(self):
+        return f'<Trip lat: {self.latitude} long {self.longitude}>'
