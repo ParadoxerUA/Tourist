d4829caa9a433a8213cc14bba7c46c9344e0881b
Cyryl Vakulenko <wither04@gmail.com>
2019-10-24 17:10:20 +0300

    remove mail_service

--- mail_service/config.py
+++
@@ -1,8 +0,0 @@
-class MailServiceConfig:
-    DEBUG = True
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
-    MAIL_USERNAME = 'kav993@gmail.com'
-    MAIL_PASSWORD = 'bqibanxccrgxqsqy'
