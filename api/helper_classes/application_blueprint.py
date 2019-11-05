74e34cb44dfed4dd7dff5bf3aa05c299801abbe6
Cyryl Vakulenko <wither04@gmail.com>
2019-11-05 16:10:45 +0200

    update setup_urls

--- api/helper_classes/application_blueprint.py
+++ api/helper_classes/application_blueprint.py
@@ -4,17 +4,31 @@ from helper_classes.model_registry import ModelRegistry
 
 
 class ApplicationBlueprint(Blueprint):
-    def __init__(self, name, import_name, controllers_list, urls_dict, models_list=None):
+    def __init__(self, name, import_name, controllers_list, urls_dict, models_list=None, *, methods=None):
         super().__init__(name, import_name)
         self.controllers_list = controllers_list
         self.models_list = models_list
         self.urls_dict = urls_dict
         self.controllers = None
         self.name = name
+        self.methods = methods
 
-    def setup_urls(self):
+    def _setup_plain_urls(self):
         for rule, class_view in self.urls_dict.items():
             self.add_url_rule(rule, view_func=class_view)
+
+    def _setup_method_urls(self):
+        for rule, class_view in self.urls_dict.items():
+            if self.methods.get(rule):
+                self.add_url_rule(rule, view_func=class_view, methods=self.methods[rule])
+            else:   
+                self.add_url_rule(rule, view_func=class_view)
+
+    def setup_urls(self):
+        if self.methods is None:
+            self._setup_plain_urls()
+        else:
+            self._setup_method_urls()
 
     def setup_controllers(self):
         controller_registry = ControllerRegistry()
