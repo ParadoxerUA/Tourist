fa1d2a3f7442e2d1ee6128e95ff38729e4b550e9
helga <olgasulymenko@gmail.com>
2019-11-07 17:50:13 +0200

    create/show/delete roles

--- api/apps/role_app/urls.py
+++ api/apps/role_app/urls.py
@@ -1,5 +1,13 @@
 from . import views
 
 urls = {
-    '/v1/role': views.RoleView.as_view('role')
+    '/v1/role/<int:trip_id>': views.RoleView.as_view('get_roles'),
+    '/v1/role': views.RoleView.as_view('add_role'),
+    '/v1/role/<int:trip_id>/<string:role_name>': views.RoleView.as_view('delete_role'),
+}
+
+methods = {
+    '/v1/role/<int:trip_id>': ['GET'],
+    '/v1/role': ['POST'],
+    '/v1/role/<int:trip_id>/<string:role_name>': ['DELETE']
 }
