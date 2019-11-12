ff38718223885bfe58ebb42e06188735a1336361
Kirill Redin <redinkirill.ua@gmail.com>
2019-11-12 16:16:46 +0200

    Remove UserProfileController from controllers list

--- api/apps/user_app/controllers/__init__.py
+++ api/apps/user_app/controllers/__init__.py
@@ -1,10 +1,8 @@
 from .user_controller import UserController
 from .login_controller import LoginController
-from .user_profile_controller import UserProfileController
 
 
 controllers = [
     UserController,
     LoginController,
-    UserProfileController,
 ]
