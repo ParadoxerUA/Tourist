97d8ad891e328a1d1c7bbef86c61890999c50d27
Bogdan Lomako <lomakojob@gmail.com>
2019-11-14 17:43:37 +0200

    added color field to role table

--- api/apps/role_app/models/models.py
+++ api/apps/role_app/models/models.py
@@ -7,6 +7,7 @@ class Role(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
+    color = db.Column(db.String(7), nullable=False)
     trip_id = db.Column(db.Integer, db.ForeignKey('trip.trip_id'), nullable=False)
 
     def __repr__(self):
