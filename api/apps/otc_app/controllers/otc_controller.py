696487fe8fa42d6f1042d2dd87f0239d2dfe88a7
Cyryl Vakulenko <wither04@gmail.com>
2019-11-18 17:10:08 +0200

    add redis support

--- api/apps/otc_app/controllers/otc_controller.py
+++ api/apps/otc_app/controllers/otc_controller.py
@@ -2,23 +2,40 @@ from apps.otc_app.otc import registration_otc
 from apps.otc_app.otc.otc_exceptions import *
 from flask import current_app
 from datetime import datetime
+import redis
 
 
-class OTCController():
+class OtcController():
+    redis_client = None
+
     @classmethod
-    def handle_uuid(cls, uuid, otc_type):
+    def handle_uuid(cls, uuid):
+        cls.redis_client = redis.Redis()
+        otc_type = cls.redis_client.get(uuid)
+    
+        if otc_type == False:
+            raise OtcNoneError('specified uuid does not exist')
+
+        otc_type = otc_type.decode('utf-8')
         if otc_type == 'user_registration':
-            try:
-                return cls._activate_user(uuid)
-            except OTCOutdatedError:
-                return 'uuid outdated'
-        else:
-            raise OTCTypeError()
+            return cls._handle_user_registration_uuid(uuid)
+
+        raise OtcTypeError
+
+    @classmethod
+    def _handle_user_registration_uuid(cls, uuid):
+        try:
+            action_result = cls._activate_user(uuid)
+            cls.redis_client.delete(uuid)
+            return action_result
+        except OtcOutdatedError:
+            return 'uuid outdated'
 
     @classmethod
     def create_registration_uuid(cls):
-        otc = registration_otc.RegistrationOTC()
+        otc = registration_otc.RegistrationOtc()
         otc.create_otc()
+        otc.add_otc_to_redis()
         return otc.get_otc()
 
     @classmethod
@@ -29,4 +46,4 @@ class OTCController():
         if user.is_uuid_valid():
             user.activate_user()
             return 'user activated'
-        raise OTCOutdatedError()
+        raise OtcOutdatedError
