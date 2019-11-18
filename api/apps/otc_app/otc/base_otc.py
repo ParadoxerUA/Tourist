d4b0d7eeb22a2bc49205267f52d8604740662d41
Cyryl Vakulenko <wither04@gmail.com>
2019-11-18 15:24:08 +0200

    add insertion to redis

--- api/apps/otc_app/otc/base_otc.py
+++ api/apps/otc_app/otc/base_otc.py
@@ -1,4 +1,5 @@
 from .otc_exceptions import OTCNoneError
+import redis
 
 class BaseOTC:
     def __init__(self, otc_type):
@@ -8,10 +9,18 @@ class BaseOTC:
     def get_otc(self):
         if self._code:
             return self._code
-        raise OTCNoneError()
+        raise OTCNoneError
 
     def get_otc_type(self):
         return self._otc_type
+
+    def add_otc_to_redis(self):
+        if not self._code:
+            raise OTCNoneError
+        with redis.Redis() as redis_client:
+            res = redis_client.set(self._code, self._otc_type)
+            if res == False:
+                raise Exception("redis insertion failed")
 
     def create_otc(self):
         raise NotImplementedError
