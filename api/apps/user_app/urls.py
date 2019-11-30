ce1aa121867cce97e9f1064fdf5ebcf83c5d559a
Mazzart <am.magicsmile@gmail.com>
2019-11-30 23:10:08 +0200

    Add url to get user roles

--- api/apps/user_app/urls.py
+++ api/apps/user_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urls = {
     '/v1/user': views.UserView.as_view('user'),
     '/v1/user/trips': views.UserTripsView.as_view('get_user_trips'),
+    '/v1/user/roles': views.UserRolesView.as_view('get_user_roles'),
     '/v1/login': views.LoginView.as_view('login'),
     '/v1/logout': views.LogoutView.as_view('logout'),
 }
