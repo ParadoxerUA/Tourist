94e5b5697b5ad25125a87973fa7cb23ff0de5c41
Alex Shmidt <kav993@gmail.com>
2019-12-10 14:10:48 +0200

    add EquipmentView func

--- api/apps/equipment_app/views.py
+++ api/apps/equipment_app/views.py
@@ -9,29 +9,26 @@ class EquipmentView(BaseView):
     def __init__(self):
         self.equipment_controller = current_app.blueprints['equipment'].controllers.EquipmentController
 
+    @login_required
     def get(self, equipment_id):
-        try:
-            data = self.equipment_controller.get_equipment_data(equipment_id)
-        except ValidationError as err:
-            return self._get_response(data=err.messages, status_code=400)
+        response, status_code = self.equipment_controller.get_equipment_data(equipment_id)
+        return self._get_response(response, status_code=status_code)
 
-        return self._get_response(data, status_code=200)
-
-    def patch(self, equipment_id):
+    @login_required
+    def put(self, equipment_id):
         try:
             new_equipment_data = EquipmentSchema().load(request.json)
             data = self.equipment_controller.update_equipment(equipment_id, new_equipment_data)
         except ValidationError as err:
             return self._get_response(data=err.messages, status_code=400)
-
         return self._get_response("Successfully updated", status_code=200)
 
+    @login_required
     def delete(self, equipment_id):
         try:
             data = self.equipment_controller.delete_equipment(equipment_id)
         except ValidationError as err:
             return self._get_response(data=err.messages, status_code=400)
-
         return self._get_response("Successfully deleted", status_code=200)
 
     @login_required
@@ -40,5 +37,5 @@ class EquipmentView(BaseView):
             equipment_data = EquipmentSchema().load(request.json)
         except ValidationError as err:
             return self._get_response(err.messages, status_code=400)
-        response = self.equipment_controller.create_equipment(equipment_data)
-        return self._get_response(response, status_code=201)
+        response, status_code = self.equipment_controller.create_equipment(equipment_data)
+        return self._get_response(response, status_code=status_code)
