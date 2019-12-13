9154673bb84d1f80d90ed4e1487c85bfea7c0165
helga <olgasulymenko@gmail.com>
2019-12-13 17:30:14 +0200

    toggle roles when left trip

--- api/apps/user_app/controllers/user_controller.py
+++ api/apps/user_app/controllers/user_controller.py
@@ -14,6 +14,11 @@ class UserController:
         user = current_app.models.User.get_user_by_id(user_id)
         return user
 
+    @staticmethod
+    def _get_trip(trip_id):
+        trip = current_app.models.Trip.get_trip_by_id(trip_id)
+        return trip
+
     # tofix
     @classmethod
     def register_user(cls, name, email, password, surname=None):
@@ -82,7 +87,7 @@ class UserController:
 
     @classmethod
     def delete_user_from_trip(cls, trip_id, user_to_delete):
-        trip = current_app.models.Trip.get_trip_by_id(trip_id=trip_id)
+        trip = cls._get_trip(trip_id=trip_id)
         if (user_to_delete != g.user_id) and (g.user_id != trip.admin_id):
             return 'You have no rights', 400
         if (g.user_id == trip.admin_id) and (user_to_delete == g.user_id):
@@ -93,6 +98,10 @@ class UserController:
             trip.delete_user(user),
             return trip.delete_trip(), 201
         user = cls._get_user(user_to_delete)
+        roles = user.get_fields(['roles'], trip_id=trip_id)
+        if roles:
+            for role in roles['roles']:
+                role.toggle_role(user)
         return trip.delete_user(user), 201
 
     @classmethod
