6ef042f0fc4fb77a72c21c92f4fdf9534588005a
Kirill Redin <redinkirill.ua@gmail.com>
2019-12-18 16:37:16 +0200

    Fix equipment and otc tests

--- tests/unittests/test_api/test_apps/test_equipment_app/test_controllers/test_equipment_controller.py
+++ tests/unittests/test_api/test_apps/test_equipment_app/test_controllers/test_equipment_controller.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock
 from tests.unittests.basic_test import BasicTest
 import sys
 
@@ -7,10 +7,16 @@ if "./api" not in sys.path:
 
 from apps.equipment_app.controllers.equipment_controller import EquipmentController
 from apps.equipment_app.models import Equipment
+# from apps.user_app.models.user_model import User
 
 
 class TestEquipmentController(BasicTest):
     """Tests for equipment_app controller"""
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        from flask import g
+        g.user_id = Mock()
 
     def setUp(self):
         self.Equipment = EquipmentController()
@@ -22,39 +28,43 @@ class TestEquipmentController(BasicTest):
             'trip_id': 1
         }
 
-    # @patch.object(Equipment, 'get_equipment_by_id')
-    # def test_get_equipment_data(self, get_equipment_by_id):
-    #     equipment_mock = Mock()
-    #     equipment_mock.equipment_id = self.equipment_data['equipment_id']
-    #     equipment_mock.name = self.equipment_data['name']
-    #     equipment_mock.weight = self.equipment_data['weight']
-    #     equipment_mock.quantity = self.equipment_data['quantity']
-    #     equipment_mock.trip_id = self.equipment_data['trip_id']
+    @patch.object(Equipment, 'get_equipment_by_id')
+    def test_get_equipment_data(self, get_equipment_by_id):
+        user = Mock()
+        user.trips = []
+        EquipmentController._get_user = Mock(return_value=user)
+        equipment_mock = Mock(**self.equipment_data)
 
-    #     get_equipment_by_id.return_value = equipment_mock
-    #     result = self.Equipment.get_equipment_data(equipment_id=1)
+        user.trips.append(equipment_mock.trip)
 
-    #     self.assertEqual(result, equipment_mock)
+        get_equipment_by_id.return_value = equipment_mock
+        result = self.Equipment.get_equipment_data(equipment_id=1)
 
-    # @patch.object(Equipment, 'update_equipment')
-    # def test_update_equipment(self, update_equipment):
-    #     new_data = {
-    #         'name': 'Orange',
-    #         'weight': 2,
-    #         'quantity': 4
-    #     }
-    #     update_equipment.return_value = None
-    #     equipment_id = 1
-    #     result = self.Equipment.update_equipment(equipment_id, new_data)
+        self.assertEqual(result, (equipment_mock, 201))
 
-    #     self.assertEqual(result, None)
+    @patch.object(Equipment, 'update_equipment')
+    def test_update_equipment(self, update_equipment):
+        new_data = {
+            'name': 'Orange',
+            'weight': 2,
+            'quantity': 4
+        }
+        EquipmentController._get_eq = Mock()
+        EquipmentController._user_has_privileges = Mock(return_value=True)
+        update_equipment.return_value = None
+        equipment_id = 1
+        result = self.Equipment.update_equipment(equipment_id, new_data)
+
+        self.assertEqual(result, (None, 201))
 
     # @patch.object(Equipment, 'delete_equipment')
     # def test_delete_equipment(self, delete_equipment):
+    #     EquipmentController._get_eq = Mock()
+    #     EquipmentController._user_has_privileges = Mock(return_value=True)
     #     delete_equipment.return_value = None
     #     result = self.Equipment.delete_equipment(equipment_id=1)
 
-    #     self.assertEqual(result, None)
+    #     self.assertEqual(result, (None, 201))
 
     # @patch.object(Equipment, 'create_equipment')
     # def test_create_equipment(self, create_equipment):
--- tests/unittests/test_api/test_apps/test_otc_app/test_controllers/test_otc_controller.py
+++ tests/unittests/test_api/test_apps/test_otc_app/test_controllers/test_otc_controller.py
@@ -1,37 +1,43 @@
 import unittest
 from unittest.mock import Mock, MagicMock, patch
 from tests.unittests.basic_test import BasicTest
-from apps.user_app.controllers.user_controller import UserController
-from apps.trip_app.controllers.trip_controller import TripController 
 import redis
 
 
 class TestOtcController(BasicTest):
-    pass
-    # def test_handle_uuid(self):
-    #     from apps.otc_app.otc import otc_exceptions
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
+        cls.UserController = cls.app.blueprints['user'].controllers.UserController
+        cls.TripController = cls.app.blueprints['trip'].controllers.TripController
+        cls.OtcController = cls.app.blueprints['otc'].controllers.OtcController
 
-    #     redis_client = redis.Redis()
-    #     redis_client.set('uuid1', 'user_registration')
-    #     redis_client.set('uuid2', 'trip_link')
-    #     redis_client.set('uuid', 'some_type')
+    def test_handle_uuid(self):
+        from apps.otc_app.otc import otc_exceptions
+        from flask import g
 
-    #     with patch.object(UserController, 'activate_user', return_value='user_registration'):
-    #         self.assertEqual(
-    #             self.app.blueprints['otc'].controllers.OtcController.handle_uuid('uuid1'),
-    #             'user_registration'
-    #         )
+        g.user_id = Mock()
+        redis_client = redis.Redis()
+        redis_client.set('uuid1', 'user_registration')
+        redis_client.set('uuid2', 'trip_link')
+        redis_client.set('uuid', 'some_type')
 
-    #     with patch.object(TripController, 'user_to_trip', return_value='trip_link'):
-    #         self.assertEqual(
-    #             self.app.blueprints['otc'].controllers.OtcController.handle_uuid('uuid2'),
-    #             'trip_link'
-    #         )
+        with patch.object(self.UserController, 'activate_user', return_value='user_registration'):
+            self.assertEqual(
+                self.OtcController.handle_uuid('uuid1'),
+                'user_registration'
+            )
+
+        with patch.object(self.TripController, 'user_to_trip', return_value='trip_link'):
+            self.assertEqual(
+                self.OtcController.handle_uuid('uuid2'),
+                'trip_link'
+            )
             
-    #     with self.assertRaises(otc_exceptions.OtcTypeError):
-    #         self.app.blueprints['otc'].controllers.OtcController.handle_uuid('uuid')
+        with self.assertRaises(otc_exceptions.OtcTypeError):
+            self.app.blueprints['otc'].controllers.OtcController.handle_uuid('uuid')
 
-    #     redis_client.delete('uuid')
-    #     redis_client.delete('uuid1')
-    #     redis_client.delete('uuid2')
+        redis_client.delete('uuid')
+        redis_client.delete('uuid1')
+        redis_client.delete('uuid2')
