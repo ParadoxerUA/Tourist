361d742f4d1973adf6fb1f55ac458dfe978dac2e
Alex Shmidt <kav993@gmail.com>
2019-12-19 16:55:59 +0200

    remove item with quantity 0 from user.get_fields response

--- api/apps/user_app/models/user_model.py
+++ api/apps/user_app/models/user_model.py
@@ -127,7 +127,7 @@ class User(db.Model):
                 public_data[field] = [trip.get_trip_details(self.user_id) for trip in getattr(self, field)]
             elif field == 'equipment':
                 item_list = getattr(self, field).filter_by(trip_id=trip_id).all()
-                public_data[field] = [item.get_quantity(self.user_id) for item in item_list]
+                public_data[field] = [item.get_quantity(self.user_id) for item in item_list if item.get_quantity(self.user_id).quantity > 0]
             elif field == 'personal_stuff':
                 public_data[field] = [item for item in getattr(self, field) if item.trip_id == trip_id]
             else:
